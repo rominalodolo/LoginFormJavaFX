be322b5f9727404aa9a88ed1b8db349d0581bf98
Romina <lodoloromina@gmail.com>
2022-07-04 15:12:39 +0200

    update

--- src/main/java/com/example/loginformjavafx/HelloController.java
+++ src/main/java/com/example/loginformjavafx/HelloController.java
@@ -9,6 +9,9 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
+
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+
 }
--- src/main/java/com/example/loginformjavafx/Main.java
+++ src/main/java/com/example/loginformjavafx/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 import java.io.IOException;
 
 public class Main extends Application {
@@ -18,6 +19,6 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
