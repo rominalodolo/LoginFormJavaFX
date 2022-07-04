cc90dc3966c5faded6a60d8edea5be372030b84a
Romina <lodoloromina@gmail.com>
2022-07-04 16:46:11 +0200

    update

--- src/main/java/com/example/loginformjavafx/Main.java
+++ src/main/java/com/example/loginformjavafx/Main.java
@@ -1,14 +1,14 @@
  package com.example.loginformjavafx;
 
-import javafx.application.Application;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+ import javafx.application.Application;
+ import javafx.fxml.FXMLLoader;
+ import javafx.fxml.FXML;
+ import javafx.scene.Parent;
+ import javafx.scene.Scene;
+ import javafx.stage.Stage;
 
 
-import java.io.IOException;
+ import java.io.IOException;
 
 public class Main extends Application {
 
