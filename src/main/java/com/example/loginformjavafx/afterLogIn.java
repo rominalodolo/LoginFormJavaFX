c570104e9a989af25aaadb40f8ddb9e30132ab56
Romina <lodoloromina@gmail.com>
2022-07-04 16:26:57 +0200

    update

--- src/main/java/com/example/loginformjavafx/afterLogIn.java
+++ src/main/java/com/example/loginformjavafx/afterLogIn.java
@@ -1,5 +1,13 @@
 package com.example.loginformjavafx;
 
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
 public class afterLogIn {
-    
+
+    public void userLogOut(ActionEvent event) throws IOException{
+        Main m = new Main();
+        m.changeScene("hello-view.fxml");
+    }
 }
