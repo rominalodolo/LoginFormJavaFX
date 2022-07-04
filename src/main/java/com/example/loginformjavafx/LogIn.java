42d74b0c98c33223d4d68c39d5ffaaa904c7575e
Romina <lodoloromina@gmail.com>
2022-07-04 15:31:44 +0200

    update

--- src/main/java/com/example/loginformjavafx/LogIn.java
+++ src/main/java/com/example/loginformjavafx/LogIn.java
@@ -1,5 +1,39 @@
 package com.example.loginformjavafx;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
 public class LogIn {
+
+    public LogIn() {
+
+    }
+
+    @FXML
+    private Button button;
+    @FXML
+    private Label wrongLogIn;
+    @FXML
+    private TextField username;
+    @FXML
+    PasswordField password;
+
+    public void userLogin() throws IOException {
+        checkLogin();
+    }
+
+    private void checkLogin() throws IOException {
+        Main m = new Main();
+        if(username.getText().toString().equals("javacoding") && password.getText().toString().equals("123")){
+            wrongLogIn.setText("Success!");
+
+            m.changeScene("afterLogin")
+        }
+    }
 
 }
